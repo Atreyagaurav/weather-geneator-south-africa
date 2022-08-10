b1f57ce968dd4ef41866ea33ace283ddc39f20f9
Gaurav-Lab-PC <atreyagv@mail.uc.edu>
2022-08-10 14:22:13 -0400

    Move function body to outside

--- weather-gen/identify.simulate.WRs_markovians.R
+++ weather-gen/identify.simulate.WRs_markovians.R
@@ -11,30 +11,90 @@ library(biwavelet)
 library(parallel)
 
 
-#-- NOTE: Run auxiliary function "fit.NHMMs.depmix" beforehand -- #
-#Aux#
-#// Auxiliary function for NHMMs run
-fit.long.NHMMs.depmix <- function(my.nstates,
-                             my.num.sim,
-                             my.synoptic.pcs,
-                             my.dates.vec,
-                             num.iteration.hmms,
-                             modeltype,
-                             init.pars) {
-  
-  #'  ---------- # s-NHMM for Annual WRs Simulations -------------------------------
-  #'  Fits a non-homogeneous HMM with Seasonality  using 
-  #'  depmixs4 package and simulates 
-  #'  markov chains and response models with Seasonality 
-  #'  
-  #'  Arguments: 
-  #' 
-  #'  my.nstates : Number of hidden states of Weather Regimes
-  #'  my.num.sim : Number of simulation of WRs (1 set of t (number of years), 2 set of t, etc)
-  #'  my.synoptic.pcs : "Observed" series of PCs for geopotential heights 
-  #'  my.dates.vec : vector of dates 
-  #'  num.iteration.msar : number of simulation of Markov Chain 
-  #'  modeltype : annual, seasonal, interannual
+#1#
+#// Load processed 500mb-GPH hgt region data and dates
+hgt.synoptic.region <- readRDS(file='hgt.500.Pacific.NorthAmer.synoptic.region_19480101_20211231.rds')
+
+# for a specific WR number
+# e.g, 10 PCs
+#start_date="1948-01-01"; end_date="2021-12-31" # for TID/HFAM
+start_date="1950-01-01"; end_date="2013-12-31" # for CA Watershed Study
+first.date.weather <- as.Date(start_date); last.date.weather <- as.Date(end_date)
+dates.weather <- seq(as.Date(start_date),as.Date(end_date), by="days")
+
+start_date_synoptic="1948-01-01"; end_date_synoptic="2021-12-31"
+dates.synoptic <- seq(as.Date(start_date_synoptic),as.Date(end_date_synoptic), by="days")
+
+identical.dates.idx <- dates.synoptic%in%dates.weather
+hgt.synoptic.region <- hgt.synoptic.region[identical.dates.idx,]
+
+
+#2#
+##/ Use PCA beforehand
+hgt.synoptic.region.pca <- prcomp(hgt.synoptic.region,center=T,scale=T)
+num_eofs <- 10
+synoptic.pcs <- hgt.synoptic.region.pca$x[,1:num_eofs]
+
+
+#3#
+##/ HMMs runs followed by s-NHMMs
+# for a specific WR number
+num.states = 13 # WRs number
+number.years.long <- 1000 # e.g., 1000 years; 2000 years, etc
+my.num.sim = ceiling(number.years.long/length(unique(format(dates.synoptic[identical.dates.idx],'%Y')))) # number of chunks of historical periods; e.g., 1 is one set of simulation equal to the historical
+number.years.long2 <- my.num.sim*length(unique(format(dates.synoptic[identical.dates.idx],'%Y')))
+num.iteration.hmms = 5 # number of iteration to simulate WRs
+lst.WRs.sNHMMs.states <- list()
+# HMMs outputs
+# e.g, 10 PCs
+modHMMs <- depmix(list(PC1~1,PC2~1,PC3~1, PC4~1, PC5~1, PC6~1, PC7~1, PC8~1,
+                       PC9~1, PC10~1),
+                  nstates = num.states,
+                  family=list(gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),
+                              gaussian(),gaussian()),
+                  ntimes =  nrow(synoptic.pcs),
+                  data = data.frame(synoptic.pcs))
+fit.modHMMs.depmix <- fit(modHMMs)
+# synoptic.state.assignments <- posterior(fit.modHMMs.depmix)$state # state sequence (using the Viterbi algorithm) #
+# weather.state.assignments <- synoptic.state.assignments[dates.synoptic%in%dates.weather]   #state assignments associated with the local weather variables (in case they span a different time period)
+
+#s-NHMMs run
+hhmod <- fit.modHMMs.depmix
+hhpars <- c(unlist(getpars(hhmod)))
+hhconMat <- hhmod@conMat
+init.pars <- list()
+init.pars[['transition']] <- lapply(hhmod@transition,
+                                    function(x) x@parameters$coefficients)
+init.pars[['prior']]  <- hhmod@prior@parameters$coefficients #  prob.kmeans.list[[p]]  #
+init.pars[['response']] <- lapply(hhmod@response,
+                                  function(x) lapply(x, function(y) unlist(y@parameters)))
+init.pars[['conMat']] <- hhconMat
+
+## s-NHMM on PCs with seasonality
+## function parameters
+my.nstates <- num.states
+my.num.sim <- my.num.sim
+my.synoptic.pcs <- synoptic.pcs
+my.dates.vec <- dates.synoptic[identical.dates.idx]
+num.iteration.hmms
+modeltype <- 'annual'
+init.pars <- init.pars
+
+## prev function body
+
+#'  ---------- # s-NHMM for Annual WRs Simulations -------------------------------
+#'  Fits a non-homogeneous HMM with Seasonality  using 
+#'  depmixs4 package and simulates 
+#'  markov chains and response models with Seasonality 
+#'  
+#'  Arguments: 
+#' 
+#'  my.nstates : Number of hidden states of Weather Regimes
+#'  my.num.sim : Number of simulation of WRs (1 set of t (number of years), 2 set of t, etc)
+#'  my.synoptic.pcs : "Observed" series of PCs for geopotential heights 
+#'  my.dates.vec : vector of dates 
+#'  num.iteration.msar : number of simulation of Markov Chain 
+#'  modeltype : annual, seasonal, interannual
 
 
   # Prepare simulation output: [number of days (t) * number of sets of simulated WRs (entire stretch, t)] x number of ensemble members
@@ -257,82 +317,12 @@ fit.long.NHMMs.depmix <- function(my.nstates,
     if(prct.done%%5 == 0){print(paste("---finishing ",it.cnt,"out of",num.iteration.hmms," simulations -----:",prct.done,"%"))}
   }
   
-  return(list(matrix.hmms.seq.states=matrix.hmms.seq.states,
-              fitted.model = fmod.depmix,
-              viterbi.seq = seq.state, 
-              init.seed = init.seed))
-  
-}
-
-#1#
-#// Load processed 500mb-GPH hgt region data and dates
-hgt.synoptic.region <- readRDS(file='hgt.500.Pacific.NorthAmer.synoptic.region_19480101_20211231.rds')
-
-# for a specific WR number
-# e.g, 10 PCs
-#start_date="1948-01-01"; end_date="2021-12-31" # for TID/HFAM
-start_date="1950-01-01"; end_date="2013-12-31" # for CA Watershed Study
-first.date.weather <- as.Date(start_date); last.date.weather <- as.Date(end_date)
-dates.weather <- seq(as.Date(start_date),as.Date(end_date), by="days")
-
-start_date_synoptic="1948-01-01"; end_date_synoptic="2021-12-31"
-dates.synoptic <- seq(as.Date(start_date_synoptic),as.Date(end_date_synoptic), by="days")
-
-identical.dates.idx <- dates.synoptic%in%dates.weather
-hgt.synoptic.region <- hgt.synoptic.region[identical.dates.idx,]
-
-
-#2#
-##/ Use PCA beforehand
-hgt.synoptic.region.pca <- prcomp(hgt.synoptic.region,center=T,scale=T)
-num_eofs <- 10
-synoptic.pcs <- hgt.synoptic.region.pca$x[,1:num_eofs]
-
-
-#3#
-##/ HMMs runs followed by s-NHMMs
-# for a specific WR number
-num.states = 13 # WRs number
-number.years.long <- 1000 # e.g., 1000 years; 2000 years, etc
-my.num.sim = ceiling(number.years.long/length(unique(format(dates.synoptic[identical.dates.idx],'%Y')))) # number of chunks of historical periods; e.g., 1 is one set of simulation equal to the historical
-number.years.long2 <- my.num.sim*length(unique(format(dates.synoptic[identical.dates.idx],'%Y')))
-num.iteration.hmms = 5 # number of iteration to simulate WRs
-lst.WRs.sNHMMs.states <- list()
-# HMMs outputs
-# e.g, 10 PCs
-modHMMs <- depmix(list(PC1~1,PC2~1,PC3~1, PC4~1, PC5~1, PC6~1, PC7~1, PC8~1,
-                       PC9~1, PC10~1),
-                  nstates = num.states,
-                  family=list(gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),gaussian(),
-                              gaussian(),gaussian()),
-                  ntimes =  nrow(synoptic.pcs),
-                  data = data.frame(synoptic.pcs))
-fit.modHMMs.depmix <- fit(modHMMs)
-# synoptic.state.assignments <- posterior(fit.modHMMs.depmix)$state # state sequence (using the Viterbi algorithm) #
-# weather.state.assignments <- synoptic.state.assignments[dates.synoptic%in%dates.weather]   #state assignments associated with the local weather variables (in case they span a different time period)
-
-#s-NHMMs run
-hhmod <- fit.modHMMs.depmix
-hhpars <- c(unlist(getpars(hhmod)))
-hhconMat <- hhmod@conMat
-init.pars <- list()
-init.pars[['transition']] <- lapply(hhmod@transition,
-                                    function(x) x@parameters$coefficients)
-init.pars[['prior']]  <- hhmod@prior@parameters$coefficients #  prob.kmeans.list[[p]]  #
-init.pars[['response']] <- lapply(hhmod@response,
-                                  function(x) lapply(x, function(y) unlist(y@parameters)))
-init.pars[['conMat']] <- hhconMat
-
-# s-NHMM on PCs with seasonality
-tmp.list <- fit.long.NHMMs.depmix(my.nstates=num.states,
-                                  my.num.sim=my.num.sim,
-                                  my.synoptic.pcs=synoptic.pcs,
-                                  my.dates.vec=dates.synoptic[identical.dates.idx],
-                                  num.iteration.hmms,
-                                  modeltype='annual',
-                                  init.pars = init.pars)
-lst.WRs.sNHMMs.states <- tmp.list
-gc()
+lst.WRs.sNHMMs.states <- list(matrix.hmms.seq.states=matrix.hmms.seq.states,
+                              fitted.model = fmod.depmix,
+                              viterbi.seq = seq.state, 
+                              init.seed = init.seed)
+    
+gc()    
 print(paste("-- finishing: ",num.states))
 
 
